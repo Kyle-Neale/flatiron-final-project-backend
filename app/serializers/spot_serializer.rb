202e0647c452774e1ca4ae76a73b90e1d5b47247
morgannegagne <morganne.gagne@gmail.com>
2018-04-18 17:51:50 -0400

    add source to spot serializer

--- app/serializers/spot_serializer.rb
+++ app/serializers/spot_serializer.rb
@@ -1,5 +1,5 @@
 class SpotSerializer < ActiveModel::Serializer
-  attributes :id, :spot_type, :user_id
+  attributes :id, :spot_type, :user_id, :source
   belongs_to :place
   has_many :comments
   has_many :images
